fd81ead3b1cb7c06e0c8f0404579b66c021b1828
sawmonmyatoo <sawmonmyatoo@gmail.com>
2026-09-08 21:57:32 +0630

    Fix for render

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,19 @@
 // client/vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-    server: {
+
+  server: {
+    host: "0.0.0.0",
     port: 5173,
+
     proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true
-      }
-    }
-  }
+      "/api": {
+        target: "http://localhost:5001",
+        changeOrigin: true,
+      },
+    },
+  },
 });
